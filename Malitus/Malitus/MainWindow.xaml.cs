d0b86bdcbcac9eec285ca837a0f19f66b02f7ff0
yourBABKA2 <chikenev2014@mail.ru>
2022-05-17 16:31:20 +0300

    Work in progress

--- Malitus/Malitus/MainWindow.xaml.cs
+++ Malitus/Malitus/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OxyPlot;
+using OxyPlot.Series;
 
 namespace Malitus
 {
@@ -20,9 +22,31 @@ namespace Malitus
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly FunctionSeries _series = new FunctionSeries();
         public MainWindow()
         {
             InitializeComponent();
+
+            ModelPlot = new PlotModel { Title = "Биологическая модель Мальтуса" };
+            DataContext = this;
+        }
+
+        public PlotModel ModelPlot { get; private set; }
+
+        public void OnCalculateButtonClick(object sender, RoutedEventArgs e)
+        {
+            int k = int.Parse(input_k.Text);
+            float q = float.Parse(input_q.Text);
+            int years = int.Parse(input_years.Text);
+
+            float prevCount = float.Parse(input_startfish.Text);
+            for (int year = 1; year <= years; year++)
+			{
+                // кол-во рыб + рождаемость * кол-во рыб - смертность * (кол-во рыб^2) 
+                prevCount = prevCount + k * prevCount - q * (prevCount * prevCount);
+                _series.Points.Add(new DataPoint(prevCount, year));
+			}
+            ModelPlot.Series.Add(_series);
         }
     }
 }
